53e6d8da61a8281b2f0f538b494f4f06eaace315
Lázaro Wagner <lazarowagner@hotmail.com>
2025-11-09 05:57:50 -0300

    métodos customizados em NotaRepository para busca por aluno, turma e avaliação

--- backend/boletim-api/boletim/src/main/java/com/boletim/repository/NotaRepository.java
+++ backend/boletim-api/boletim/src/main/java/com/boletim/repository/NotaRepository.java
@@ -4,8 +4,17 @@ import com.boletim.model.Nota;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NotaRepository extends JpaRepository<Nota, Long> {
-    List<Nota> findByAlunoId(Long alunoId);
+
+    List<Nota> findByAlunoIdAndTurmaId(Long alunoId, Long turmaId);
+
+    Optional<Nota> findByAlunoIdAndAvaliacaoId(Long alunoId, Long avaliacaoId);
+
+    List<Nota> findByTurmaId(Long turmaId);
+
     List<Nota> findByAvaliacaoId(Long avaliacaoId);
+
+    List<Nota> findByAlunoId(Long alunoId);
 }
