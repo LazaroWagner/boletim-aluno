b7c2ebcd2f014c0b874371477cf8107e7614a86f
Lázaro Wagner <lazarowagner@hotmail.com>
2025-11-09 00:34:48 -0300

    criado os Jpa repositorios

---
+++ backend/boletim-api/boletim/src/main/java/com/boletim/repository/NotaRepository.java
@@ -0,0 +1,11 @@
+package com.boletim.repository;
+
+import com.boletim.model.Nota;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotaRepository extends JpaRepository<Nota, Long> {
+    List<Nota> findbyAlunoId(Long alunoId);
+    List<Nota> findByAvaliacaoId(Long avaliacaoId);
+}
