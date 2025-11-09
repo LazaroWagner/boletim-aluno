8d9fbeda8e406ca4a0f87bafb2bb7277cac93e7d
Lázaro Wagner <lazarowagner@hotmail.com>
2025-11-09 02:33:43 -0300

    correcao de nomenclatura

--- backend/boletim-api/boletim/src/main/java/com/boletim/repository/NotaRepository.java
+++ backend/boletim-api/boletim/src/main/java/com/boletim/repository/NotaRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface NotaRepository extends JpaRepository<Nota, Long> {
-    List<Nota> findbyAlunoId(Long alunoId);
+    List<Nota> findByAlunoId(Long alunoId);
     List<Nota> findByAvaliacaoId(Long avaliacaoId);
 }
