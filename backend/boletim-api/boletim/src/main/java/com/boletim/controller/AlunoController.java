e7f40bfcb721e90bf7915dc85e7988386d39eced
Lázaro Wagner <lazarowagner@hotmail.com>
2025-11-09 04:15:11 -0300

    criado controller e service para Aluno

---
+++ backend/boletim-api/boletim/src/main/java/com/boletim/controller/AlunoController.java
@@ -0,0 +1,65 @@
+package com.boletim.controller;
+
+import com.boletim.dto.AlunoRequest;
+import com.boletim.dto.AlunoResponse;
+import com.boletim.model.Aluno;
+import com.boletim.service.AlunoService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController@RequestMapping("/aluno")
+public class AlunoController {
+
+    private final AlunoService alunoService;
+
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Aluno>> listarTodos() {
+        return ResponseEntity.ok(alunoService.listarTodos());
+    }
+
+    @GetMapping("{/id}")
+    public ResponseEntity<Aluno> buscarId(@PathVariable Long id) {
+        Optional<Aluno> aluno = alunoService.buscarId(id);
+        return aluno.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/turma/{turmaId}")
+    public ResponseEntity<List<Aluno>> listarTurma(@PathVariable Long turmaId) {
+        return  ResponseEntity.ok(alunoService.listarTurma(turmaId));
+    }
+
+    @PostMapping
+    public ResponseEntity<AlunoResponse> criar( @Valid
+                                                @RequestBody AlunoRequest dto
+                                                ) {
+        Aluno novo = alunoService.criar(dto);
+        URI location = URI.create("/aluno/" + novo.getId());
+        return ResponseEntity.created(location).body(alunoService.toResponse(novo));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AlunoResponse> atualizar(@PathVariable Long id,
+                                                   @Valid
+                                           @RequestBody AlunoRequest dto) {
+        Optional<Aluno> atualizado = alunoService.atualizar(id, dto);
+        return atualizado.map(aluno -> ResponseEntity.ok(alunoService.toResponse(aluno)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        boolean removido = alunoService.deletar(id);
+        return removido ? ResponseEntity.noContent().build()
+                        : ResponseEntity.notFound().build();
+    }
+}
---
+++ backend/boletim-api/boletim/src/main/java/com/boletim/service/AlunoService.java
@@ -0,0 +1,79 @@
+package com.boletim.service;
+
+import com.boletim.dto.AlunoRequest;
+import com.boletim.dto.AlunoResponse;
+import com.boletim.exception.RecursoNaoEncontradoException;
+import com.boletim.model.Aluno;
+import com.boletim.model.Turma;
+import com.boletim.repository.AlunoRepository;
+import com.boletim.repository.TurmaRepository;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AlunoService {
+
+    private final AlunoRepository alunoRepository;
+    private final TurmaRepository turmaRepository;
+
+    public AlunoService(AlunoRepository alunoRepository, TurmaRepository turmaRepository) {
+        this.alunoRepository = alunoRepository;
+        this.turmaRepository = turmaRepository;
+    }
+
+
+    public List<Aluno> listarTodos() {
+        return alunoRepository.findAll();
+    }
+
+    public Optional<Aluno> buscarId(Long id) {
+        return alunoRepository.findById(id);
+    }
+
+    public List<Aluno> listarTurma(Long turmaId) {
+        return alunoRepository.findByTurmaId(turmaId);
+    }
+
+    public Aluno criar(@Valid AlunoRequest dto) {
+        Turma turma = turmaRepository.findById(dto.getTurmaId())
+                .orElseThrow(() -> new RecursoNaoEncontradoException("Turma não encotrada"));
+
+        Aluno aluno = new Aluno();
+        aluno.setNome(dto.getNome());
+        aluno.setTurma(turma);
+
+        return alunoRepository.save(aluno);
+    }
+
+
+    public Optional<Aluno> atualizar(Long id, @Valid AlunoRequest dto) {
+        return alunoRepository.findById(id)
+                .map(aluno -> {
+                    aluno.setNome(dto.getNome());
+                    Turma turma = turmaRepository.findById(dto.getTurmaId())
+                                    .orElseThrow(() -> new RecursoNaoEncontradoException("Turma nao encontrada" + dto.getTurmaId()));
+                    aluno.setTurma(turma);
+                    return alunoRepository.save(aluno);
+                });
+    }
+
+    public boolean deletar(Long id) {
+        return alunoRepository.findById(id)
+                .map(aluno -> {
+                    alunoRepository.delete(aluno);
+                    return true;
+                })
+                .orElse(false);
+    }
+
+    public AlunoResponse toResponse(Aluno aluno) {
+        return new AlunoResponse(
+                aluno.getId(),
+                aluno.getNome(),
+                aluno.getTurma().getNome()
+        );
+    }
+}
